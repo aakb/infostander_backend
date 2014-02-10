391449d060ad2947585acc5189493d602a5c3ce3
Troels Ugilt Jensen <tujensen@gmail.com>
2014-02-10 09:49:55 +0100

    Moved toggle show_archived to cookie

--- src/Infostander/AdminBundle/Controller/SlideController.php
+++ src/Infostander/AdminBundle/Controller/SlideController.php
@@ -4,21 +4,49 @@ namespace Infostander\AdminBundle\Controller;
 
 use Infostander\AdminBundle\Entity\Slide;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 
-class SlideController extends Controller
-{
-    public function indexAction($showarchived)
-    {
-      $slides = $this->getDoctrine()->getRepository('InfostanderAdminBundle:Slide')->findAll();
-      return $this->render(
-        'InfostanderAdminBundle:Slide:index.html.twig',
-        array('slides' => $slides, 'showarchived' => $showarchived)
-      );
+class SlideController extends Controller {
+  public function indexAction(Request $request) {
+    $slides = $this->getDoctrine()
+      ->getRepository('InfostanderAdminBundle:Slide')->findBy(array(), array('title' => 'asc'));
+
+
+    // Get show archived cookie
+    $showArchived = 0;
+    $showArchivedCookie = $request->cookies->get('SHOW_ARCHIVED');
+    if (isset($showArchivedCookie)) {
+      $showArchived = $showArchivedCookie;
     }
 
-  public function addAction(Request $request)
-  {
+    return $this->render(
+      'InfostanderAdminBundle:Slide:index.html.twig',
+      array('slides' => $slides, 'showarchived' => $showArchived)
+    );
+  }
+
+  public function toggleShowArchivedAction(Request $request) {
+    // Get show archived cookie
+    $showArchived = 1;
+    $showArchivedCookie = $request->cookies->get('SHOW_ARCHIVED');
+    if (isset($showArchivedCookie)) {
+      if ($showArchivedCookie == 1)  {
+        $showArchived = 0;
+      }
+      else {
+        $showArchived = 1;
+      }
+    }
+
+    $cookie = new Cookie('SHOW_ARCHIVED', $showArchived, 0, '/', null, false, false);
+    $response = new RedirectResponse($this->generateUrl("infostander_admin_slide"));
+    $response->headers->setCookie($cookie);
+    return $response;
+  }
+
+  public function addAction(Request $request) {
     $slide = new Slide();
 
     $form = $this->createForm('slide', $slide);
@@ -28,7 +56,7 @@ class SlideController extends Controller
     if ($form->isValid()) {
       $manager = $this->getDoctrine()->getManager();
 
-      $slide->setArchived(false);
+      $slide->setArchived(FALSE);
 
       $manager->persist($slide);
       $manager->flush();
@@ -42,9 +70,10 @@ class SlideController extends Controller
   }
 
   public function deleteAction($id) {
-    $slide = $this->getDoctrine()->getRepository('InfostanderAdminBundle:Slide')->find($id);
+    $slide = $this->getDoctrine()->getRepository('InfostanderAdminBundle:Slide')
+      ->find($id);
 
-    if ($slide != null) {
+    if ($slide != NULL) {
       $manager = $this->getDoctrine()->getManager();
       $manager->remove($slide);
       $manager->flush();
@@ -53,9 +82,10 @@ class SlideController extends Controller
   }
 
   public function toggleArchivedAction($id) {
-    $slide = $this->getDoctrine()->getRepository('InfostanderAdminBundle:Slide')->find($id);
+    $slide = $this->getDoctrine()->getRepository('InfostanderAdminBundle:Slide')
+      ->find($id);
 
-    if ($slide != null) {
+    if ($slide != NULL) {
       $manager = $this->getDoctrine()->getManager();
 
       $slide->setArchived(!$slide->getArchived());
