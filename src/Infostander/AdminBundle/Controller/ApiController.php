e742004e7da652365d954381c288eb647c4c751e
Troels Ugilt Jensen <tujensen@gmail.com>
2014-02-12 15:18:51 +0100

    Changed json output from API.

--- src/Infostander/AdminBundle/Controller/ApiController.php
+++ src/Infostander/AdminBundle/Controller/ApiController.php
@@ -61,7 +61,7 @@ class ApiController extends Controller {
     $manager->flush();
 
     // Generate the response.
-    $responseData = array('statusCode'=>200, 'screenID'=>$screen->getId(), 'screenName'=>$screen->getTitle(), 'screenGroups'=>array());
+    $responseData = array('statusCode'=>200, 'id'=>$screen->getId(), 'name'=>$screen->getTitle(), 'groups'=>array());
     return new Response(json_encode($responseData));
   }
 }
